afc8777c3c42d29e84b1067685096d336a5415ec
Thomas Schaper <thomas@codegra.de>
2020-03-18 13:48:54 +0100

    Add Java13 support (#12)
    * Add Java13 support
    
    This also updates our fork of jplag to the newest (and greatest?) of the jplag
    master branch.
    
    * Update version to 2.15.0

---
+++ jplag.frontend.java-1.13/src/main/java/jplag/java113/Language.java
@@ -0,0 +1,69 @@
+package jplag.java113;
+
+import java.io.File;
+
+import jplag.ProgramI;
+import jplag.Structure;
+
+/**
+ * Hello world!
+ *
+ */
+public class Language implements jplag.Language {
+	private Parser parser;
+
+	public Language(ProgramI program) {
+		this.parser = new Parser();
+		this.parser.setProgram(program);
+
+	}
+	
+    public String[] suffixes() {
+    	String[] res = { ".java", ".JAVA" };
+		return res;
+	}
+
+	public String name() {
+		return "Javac 1.13+ based AST plugin";
+	}
+
+	public String getShortName() {
+		return "java113";
+	}
+
+	public int min_token_match() {
+		return 9;
+	}
+	public boolean supportsColumns() {
+		return true;
+	}
+
+	public boolean isPreformated() {
+		return true;
+	}
+
+	public boolean usesIndex() {
+		return false;
+	}
+
+	public int noOfTokens() {
+		return JavaToken.staticNumberOfTokens();
+	}
+
+	public String type2string(int type) {
+		return JavaToken.type2string(type);
+	}
+
+	public Structure parse(File dir, String[] files) {
+		return this.parser.parse(dir, files);
+	}
+
+	public boolean errors() {
+		return this.parser.getErrors();
+	}
+
+	public int errorsCount() {
+		return this.parser.errorsCount();
+	}
+
+}
